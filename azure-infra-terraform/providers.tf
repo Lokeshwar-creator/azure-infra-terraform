27d79fd763c521532cd114338c7addbd3f326d1e
LOKESHWAR S <118600516+Lokeshwar-creator@users.noreply.github.com>
2025-10-30 00:42:34 +0530

    Update Terraform provider version and requirements

--- azure-infra-terraform/providers.tf
+++ azure-infra-terraform/providers.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~>3.100"
     }
   }
-  required_version = ">= 1.0"
+
+  required_version = ">= 1.6.0"
 }
 
 provider "azurerm" {
