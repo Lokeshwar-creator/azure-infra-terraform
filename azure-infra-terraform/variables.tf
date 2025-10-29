399ce7994ca2c48ecfb553764a6a57af779d6d22
LOKESHWAR S <118600516+Lokeshwar-creator@users.noreply.github.com>
2025-10-30 00:43:18 +0530

    Enhance variables with descriptions and defaults
    Updated variable definitions to include descriptions and changed default values for resource group and storage account.

--- azure-infra-terraform/variables.tf
+++ azure-infra-terraform/variables.tf
@@ -1,9 +1,34 @@
 variable "resource_group_name" {
-  type    = string
-  default = "StudentIDValidationRG"
+  description = "Resource group name"
+  default     = "studentIDvalidationrg"
 }
 
 variable "location" {
-  type    = string
-  default = "East US"
+  description = "Azure region"
+  default     = "East US"
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  default     = "studentidstorageacc"
+}
+
+variable "function_app_name" {
+  description = "Azure Function App name"
+  default     = "student-id-checker-func"
+}
+
+variable "sql_server_name" {
+  description = "SQL Server name"
+  default     = "studentid"
+}
+
+variable "sql_admin_login" {
+  description = "SQL admin username"
+  default     = "sqladminuser"
+}
+
+variable "sql_admin_password" {
+  description = "SQL admin password"
+  default     = "YourStrongPassword123!"
 }
